aaf4c83e5a18c32b38d3a9155ef36a878adacae7
李连有 <li.lian.you163@163.com>
2016-08-15 15:31:42 +0800

    建立工程

---
+++ CrazyDiary/CrazyDiary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CrazyDiary
+//
+//  Created by ss-iOS-LLY on 16/8/15.
+//  Copyright © 2016年 lilianyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CrazyDiary/CrazyDiary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CrazyDiary
+//
+//  Created by ss-iOS-LLY on 16/8/15.
+//  Copyright © 2016年 lilianyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
